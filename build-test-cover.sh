6fdbb7b1af254639fe8cd52e2859d5345e5836fb
Mason Boeman <masonboeman@gmail.com>
2021-01-02 11:51:42 -0600

    remove rust calls to external actions

--- build-test-cover.sh
+++ build-test-cover.sh
@@ -4,17 +4,17 @@
 export LLVM_PROFILE_FILE="target/debug/coverage/%m.profraw"
 
 # build the test binary with coverage instrumentation
-executables=$(RUSTFLAGS="-Zinstrument-coverage" cargo test --tests --no-run --message-format=json | jq -r "select(.profile.test == true) | .executable")
+executables=$(RUSTFLAGS="-Zinstrument-coverage" cargo +nightly test --tests --no-run --message-format=json | jq -r "select(.profile.test == true) | .executable")
 
 # run instrumented tests
 $executables
 
 # combine profraw files
-cargo profdata -- merge -sparse target/debug/coverage/*.profraw -o target/debug/coverage/combined.profdata
+cargo +nightly profdata -- merge -sparse target/debug/coverage/*.profraw -o target/debug/coverage/combined.profdata
 
 # collect coverage
-cargo cov -- export $executables \
+cargo +nightly cov -- export $executables \
     --instr-profile=target/debug/coverage/combined.profdata \
     --ignore-filename-regex="$IGNORE_PATTERN" \
     --skip-functions \
-    | cargo llvm-codecov-converter > $OUTPUT_PATH
+    | cargo +nightly llvm-codecov-converter > $OUTPUT_PATH
